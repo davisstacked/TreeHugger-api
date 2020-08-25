fe111cd15f0d7715710f3f1b2bb31aa722d385a7
davisstacked <brettzvi@gmail.com>
2020-08-25 16:28:16 -0700

    seed file and community photo page is working

--- seed.js
+++ seed.js
@@ -4,25 +4,48 @@ const data = require('./userData.json');
 const photodata = require('./photoData.json');
 
 
-db.Photo.deleteMany({}, (err, deletedPhotos) => {
-    db.Photo.create(photodata.photos, (err, seededPhotos) => {
-        if (err) console.log(err);
-
-        console.log(photodata.photos.length, 'photos created successfully');
-
-        process.exit();
-    });
-});
 
 db.User.deleteMany({}, (err, deletedUsers) => {
-    db.User.create(data.users, (err, seededUsers) => {
-        if (err) console.log(err);
-
-        console.log(data.users.length, 'users created successfully');
-
-        process.exit();
+    db.Photo.create(photodata.photos, (err, seededPhotos) => {
+        for (let i=0; i<data.users.length; i++) {
+            let user = data.users[i]
+            user.photos.push(`${seededPhotos[0]._id}`) 
+            console.log(user)
+            db.User.create(user, (err, seededUser) => {
+                console.log('hello world')
+                if (err) console.log(err);
+                console.log(seededUser)
+                console.log(data.users.length, 'users created successfully');
+                if (i === data.users.length - 1) {
+                    process.exit();
+                }
+                
+            })
+        }
     });
 });
 
 // you need your database connection and model available in your seed file.
 // If you have both in your seed file, and you are using dotenv for environment vars, make sure you are requiring dotenv in your seed file. Also make sure sure the dotenv is being required before your database and model.
+
+         // {
+            //         "title": "Grand Canyon",
+            //         "image": "https://www.nps.gov/grca/planyourvisit/images/0531fyp.jpg?maxwidth=650&autorotate=false",
+            //         "caption": "Gorgeous Sunrise"
+            //     }, 
+            //     {
+            //         "title": "Grand Canyon",
+            //         "image": "https://www.nps.gov/grca/planyourvisit/images/0531fyp.jpg?maxwidth=650&autorotate=false",
+            //         "caption": "Gorgeous Sunrise"
+            //     },
+            //     {
+            //         "title": "Grand Canyon",
+            //         "image": "https://www.nps.gov/grca/planyourvisit/images/0531fyp.jpg?maxwidth=650&autorotate=false",
+            //         "caption": "Gorgeous Sunrise"
+            //     },
+            //     {
+            //         "title": "Grand Canyon",
+            //         "image": "https://www.nps.gov/grca/planyourvisit/images/0531fyp.jpg?maxwidth=650&autorotate=false",
+            //         "caption": "Gorgeous Sunrise"
+            //     }
+            // ]
